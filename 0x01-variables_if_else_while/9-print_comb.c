e77bcf6a404d5bc8a53977779a6d9d52095f99d7
lufegoce <zbytexpress@gmail.com>
2020-02-06 22:02:51 +0000

    New Project 10-print_comb2.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*main - print base16
+*Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 48; c <= 57; ++c)
+{
+putchar(c);
+if ( c != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar('\n');
+return (0);
+
+}
