92cc6aaf47d05051cea7cdc2c58edd4868be6390
lufegoce <zbytexpress@gmail.com>
2020-02-06 19:41:10 +0000

    New Project 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*main - prints the alphabet in lowercase
+*Return: 0
+*/
+int main(void)
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; ++alpha)
+putchar(alpha);
+return (0);
+
+}
