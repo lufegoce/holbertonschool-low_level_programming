abb731c110942830c0062e1af5f7973b6e0c7fdc
lufegoce <zbytexpress@gmail.com>
2020-02-06 20:32:11 +0000

    New Project 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+*main - prints the alphabet in lowercase
+*Return: 0
+*/
+int main(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+	if (c != 'q' && c != 'e')
+	{
+		putchar(c);
+	}
+}
+putchar('\n');
+return (0);
+
+}
