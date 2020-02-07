0ba4737f5a67545dde4f734382688cd3cd3cb233
lufegoce <zbytexpress@gmail.com>
2020-02-07 21:12:20 +0000

    New Project 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+*main - print base16
+*Return: 0
+*/
+int main(void)
+{
+int c;
+int j;
+int i;
+for (c = '0'; c <= '7'; c++)
+{
+	for (j = c + 1; j <= '9'; j++)
+	{
+		for (i = j + 1; i <= '9'; i++)
+		{
+		putchar(c);
+		putchar(j);
+		putchar(i);
+		if (c=='7' && j=='8' && i=='9')
+		    break;
+		putchar(44);
+		putchar(32);
+}
+}
+}
+putchar('\n');
+return (0);
+
+}
