4a7013e35a56e8021dbc9205399fe1428997f4c3
lufegoce <zbytexpress@gmail.com>
2020-02-11 17:35:39 +0000

    New Project 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*print_alphabet_x10 - function print alphabet x10
+*Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char c;
+	int cont;
+
+	cont = 0;
+	while (cont <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+		_putchar(c);
+		}
+		cont++;
+		_putchar('\n');
+	}
+}
