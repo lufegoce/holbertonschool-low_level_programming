81b14d5476d2fc9cf3f1819f1288d752990345bc
lufegoce <zbytexpress@gmail.com>
2020-02-11 19:39:36 +0000

    New Project 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+*print_sign - check for print_sign case
+*@n:caracter int
+*Return: 1 prints + if n is greater than zero
+*0 and prints 0 if n is zero or -1 and prints - if n is less than zero
+*/
+int print_sign(int n)
+{
+	if (n > 48)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 48)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
