eace5de898b91f924b2fabcd17190f380c488957
lufegoce <zbytexpress@gmail.com>
2020-02-11 17:10:58 +0000

    New Project 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+*print_alphabet - function print alphabet
+*Return: void
+*/
+void print_alphabet(void)
+{
+char c;
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+	_putchar('\n');
+}
