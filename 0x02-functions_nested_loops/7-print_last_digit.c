18733817261ef50c63ea4321c6fb6e5691a65f92
lufegoce <zbytexpress@gmail.com>
2020-02-12 02:44:49 +0000

    New Project 7-print_last_digit.c v2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,11 +6,13 @@
 */
 int print_last_digit(int n)
 {
-	int x
+	int number;
+
 	if (n < 0)
 	{
-	n = (n * -1);
+	n = n * -1;
 	}
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	number = n % 10;
+	_putchar(number + '0');
+	return (number);
 }
