c585181d04627c904d14dfacf413cbae8a71fea6
lufegoce <zbytexpress@gmail.com>
2020-02-13 16:24:47 +0000

    New Project 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*print_numbers - check for print_numbers case
+*Return: 0
+*/
+void print_numbers(void)
+{
+
+int num;
+num = 48;
+	while (num <= 57)
+	{
+	_putchar(num);
+	num++;
+	}
+	_putchar('\n');
+}
