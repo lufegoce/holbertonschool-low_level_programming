16355087cb00a91818ac610bfa28a86a963ce228
lufegoce <zbytexpress@gmail.com>
2020-02-13 18:26:07 +0000

    New Project 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+*mul - check for mul case
+*@a:caracter int
+*@b:caracter int
+*Return: 0
+*/
+int mul(int a, int b)
+{
+int mult;
+	mult = (a * b);
+	return (mult);
+}
