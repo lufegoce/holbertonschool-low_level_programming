38b357aac3314a099b1b91e3b191387cba9c1cad
lufegoce <zbytexpress@gmail.com>
2020-02-13 02:08:58 +0000

    New Porject 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+*times_table - check for times_table case
+*Return: print the times_table
+*/
+void times_table(void)
+{
+
+	int f, c, mult;
+	
+	for (f = 0; f <= 9; f++)
+	{
+		_putchar ('0');
+		for (c = 1; c <= 9; ++c)
+		{
+			mult = f * c;
+			if (mult < 10)
+			{
+			_putchar (',');
+			_putchar (' ');
+			_putchar (' ');
+			_putchar (mult + '0');
+			}
+			else 
+			{
+			_putchar (',');
+			_putchar (' ');
+			_putchar (mult / 10 + '0');
+			_putchar (mult % 10 + '0');
+			} 
+		}
+		_putchar ('\n');
+	}
+}
