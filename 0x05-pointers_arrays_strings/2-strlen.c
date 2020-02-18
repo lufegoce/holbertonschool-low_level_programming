82903c1efee210bd20de43aa16843f49032b6ed2
lufegoce <zbytexpress@gmail.com>
2020-02-18 19:47:52 +0000

    New Project 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+*_strlen - check for _strlen case
+*@s: Pointer
+*Return: s
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{
+	}
+	return (i);
+}
